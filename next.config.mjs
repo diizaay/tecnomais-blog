6810c1bd45c68b0214315c48eae5fddd3637ae7f
diizaay <divaldo511@gmail.com>
2026-03-26 15:21:50 +0100

    Monetization: Absolute definitive CSP fix with apex domains and cache busting

--- next.config.mjs
+++ next.config.mjs
@@ -33,11 +33,11 @@ const securityHeaders = [
         key: 'Content-Security-Policy',
         value: [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://5gvci.com https://quge5.com https://profitablecpmratenetwork.com https://*.profitablecpmratenetwork.com https://highperformanceformat.com https://*.highperformanceformat.com https://preferencenail.com https://*.preferencenail.com https://realizationnewestfangs.com https://*.realizationnewestfangs.com https://6opo.com https://*.6opo.com https://protrafficinspector.com https://*.protrafficinspector.com https://wayfarerorthodox.com https://*.wayfarerorthodox.com https://rtmark.net https://*.rtmark.net",
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://5gvci.com https://quge5.com https://profitablecpmratenetwork.com https://*.profitablecpmratenetwork.com https://highperformanceformat.com https://*.highperformanceformat.com https://preferencenail.com https://*.preferencenail.com https://realizationnewestfangs.com https://*.realizationnewestfangs.com https://6opo.com https://*.6opo.com https://protrafficinspector.com https://*.protrafficinspector.com https://wayfarerorthodox.com https://*.wayfarerorthodox.com https://rtmark.net https://*.rtmark.net https://adsterratechnology.com https://*.adsterratechnology.com https://onclickads.net https://*.onclickads.net https://hilltopads.com https://*.hilltopads.com",
             "worker-src 'self' blob: https://5gvci.com",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src 'self' https://fonts.gstatic.com",
-            "img-src 'self' data: blob: https://res.cloudinary.com https://images.unsplash.com https://drive.google.com https://profitablecpmratenetwork.com https://*.profitablecpmratenetwork.com https://quge5.com https://skinnycrawlinglax.com https://*.skinnycrawlinglax.com https://wayfarerorthodox.com https://*.wayfarerorthodox.com https://rtmark.net https://*.rtmark.net",
+            "img-src 'self' data: blob: https://res.cloudinary.com https://images.unsplash.com https://drive.google.com https://profitablecpmratenetwork.com https://*.profitablecpmratenetwork.com https://quge5.com https://skinnycrawlinglax.com https://*.skinnycrawlinglax.com https://wayfarerorthodox.com https://*.wayfarerorthodox.com https://rtmark.net https://*.rtmark.net https://realizationnewestfangs.com https://*.realizationnewestfangs.com",
             "connect-src 'self' https://res.cloudinary.com https://www.google-analytics.com https://5gvci.com https://quge5.com https://profitablecpmratenetwork.com https://*.profitablecpmratenetwork.com https://highperformanceformat.com https://*.highperformanceformat.com https://preferencenail.com https://*.preferencenail.com https://realizationnewestfangs.com https://*.realizationnewestfangs.com https://6opo.com https://*.6opo.com https://protrafficinspector.com https://*.protrafficinspector.com https://wayfarerorthodox.com https://*.wayfarerorthodox.com https://rtmark.net https://*.rtmark.net https://skinnycrawlinglax.com https://*.skinnycrawlinglax.com",
             "frame-src 'self' https://highperformanceformat.com https://*.highperformanceformat.com https://realizationnewestfangs.com https://*.realizationnewestfangs.com https://profitablecpmratenetwork.com https://*.profitablecpmratenetwork.com https://wayfarerorthodox.com https://*.wayfarerorthodox.com https://rtmark.net https://*.rtmark.net",
             "child-src 'self' https://highperformanceformat.com https://*.highperformanceformat.com https://realizationnewestfangs.com https://*.realizationnewestfangs.com https://profitablecpmratenetwork.com https://*.profitablecpmratenetwork.com https://wayfarerorthodox.com https://*.wayfarerorthodox.com https://rtmark.net https://*.rtmark.net",
@@ -103,6 +103,5 @@ const nextConfig = {
     },
 };
 
+// Force redeploy and cache invalidation: 2026-03-26T14:15:00Z
 export default nextConfig;
-// Trigger redeploy of stable English version
-
